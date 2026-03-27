0cce850c4e848f80ee8434dd1d1db81436991b5a
Ксения <fomichevakr.22@gmail.com>
2026-03-27 16:09:00 +0300

    add debug endpoint

--- main.py
+++ main.py
@@ -19,3 +19,7 @@ def echo():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+@app.route('/debug')
+def debug():
+    return "NEW VERSION"
